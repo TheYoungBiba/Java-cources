7c4b54018287e6141641ede9aa5536f7ac44955d
TheYoungBiba <89189450996@mail.ru>
2023-11-13 20:24:18 +0300

    Task 8 with tests

--- src/main/java/edu/hw5/Task8.java
+++ src/main/java/edu/hw5/Task8.java
@@ -11,17 +11,23 @@ public class Task8 {
         return input.matches("^(0([0,1]{2})*)|(1[0,1]([0,1]{2})*)$");
     }
 
-
-
-
-
-
-
     public static boolean zeroTree(String input) {
-        return input.matches("(((1*)0(1*)0(1*)0(1*))+)|(1+)");
+        return input.matches("^((1*)0(1*)0(1*)0(1*))+$");
     }
 
     public static boolean not11Or111(String input) {
-        return input.matches("^([^11])|([^111]$)");
+        return !input.matches("^(11|111)$");
+    }
+
+    public static boolean oddIndexEq1(String input) {
+        return input.matches("^(1[0,1])+|1([0,1]1)*$");
+    }
+
+    public static boolean containsTwo0One1(String input) {
+        return input.matches("^0*(100|010|001|00)0*$");
+    }
+
+    public static boolean notStreamOf1(String input) {
+        return input.matches("^0|1|(0+[0,1])+|([0,1]0+)+$");
     }
 }
--- src/test/java/edu/hw5/Task8Test.java
+++ src/test/java/edu/hw5/Task8Test.java
@@ -47,28 +47,23 @@ public class Task8Test {
     @CsvSource({
         "111, false",
         "11, false",
-        "1, true",
-        "10110, true",
-        "00, true",
-        "0111, true",
-        "00010111, true",
-        "0, true",
+        "1, false",
+        "10110, false",
+        "00, false",
+        "0111, false",
+        "0, false",
+        "0011000, false",
+        "10, false",
+        "1111, false",
+        "1001011100, false",
         "00110, true",
-        "0011000, true",
-        "10, true",
-        "1111, true",
+        "0001111, true",
         "10010111, true",
-        "1001011100, true",
+        "10010111000, true"
     })
     void zeroTreeTest(String testCase, boolean referent) {
         assertEquals(referent, Task8.zeroTree(testCase));
     }
-
-
-
-
-
-
 
     @ParameterizedTest
     @CsvSource({
@@ -83,11 +78,77 @@ public class Task8Test {
         "00110, true",
         "0011000, true",
         "10, true",
-        "1111, true",
+        "11111, true",
         "10010111, true",
         "1001011100, true",
     })
     void not11Or111Test(String testCase, boolean referent) {
         assertEquals(referent, Task8.not11Or111(testCase));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "00, false",
+        "0111, false",
+        "00010111, false",
+        "0, false",
+        "00110, false",
+        "0011000, false",
+        "10110111, false",
+        "1001011100, false",
+        "1, true",
+        "101111, true",
+        "10, true",
+        "1111, true",
+        "111, true",
+        "11, true",
+    })
+    void oddIndexEq1Test(String testCase, boolean referent) {
+        assertEquals(referent, Task8.oddIndexEq1(testCase));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "00, true",
+        "100, true",
+        "010, true",
+        "001, true",
+        "0000000, true",
+        "00000010000, true",
+        "10, false",
+        "1, false",
+        "0111, false",
+        "00010111, false",
+        "00110, false",
+        "0011000, false",
+        "10110111, false",
+        "1001011100, false",
+    })
+    void containsTwo0One1Test(String testCase, boolean referent) {
+        assertEquals(referent, Task8.containsTwo0One1(testCase));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "1, true",
+        "0, true",
+        "10000, true",
+        "00001, true",
+        "0000100, true",
+        "00, true",
+        "11, false",
+        "111, false",
+        "11, false",
+        "10110, false",
+        "0111, false",
+        "00010111, false",
+        "00110, false",
+        "0011000, false",
+        "11111, false",
+        "10010111, false",
+        "1001011100, false",
+    })
+    void notStreamOf1Test(String testCase, boolean referent) {
+        assertEquals(referent, Task8.notStreamOf1(testCase));
     }
 }
