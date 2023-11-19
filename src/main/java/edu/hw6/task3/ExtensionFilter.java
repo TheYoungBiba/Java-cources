deedcf69d561615eed5f8551c68578864d55ede3
TheYoungBiba <89189450996@mail.ru>
2023-11-19 23:43:22 +0300

    Completed task

--- src/main/java/edu/hw6/task3/ExtensionFilter.java
+++ src/main/java/edu/hw6/task3/ExtensionFilter.java
@@ -3,7 +3,7 @@ package edu.hw6.task3;
 import java.nio.file.FileSystems;
 import java.nio.file.PathMatcher;
 
-public interface ExtensionFilter extends AbstractFilter{
+public interface ExtensionFilter extends AbstractFilter {
     static AbstractFilter globMatches(String glob) {
         return path -> {
             PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + glob);
--- src/main/java/edu/hw6/task3/NameFilter.java
+++ src/main/java/edu/hw6/task3/NameFilter.java
@@ -3,7 +3,7 @@ package edu.hw6.task3;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public interface NameFilter extends AbstractFilter{
+public interface NameFilter extends AbstractFilter {
     static AttributeFilter regexContains(String regEx) {
         Pattern pattern = Pattern.compile(regEx);
         return path -> {
